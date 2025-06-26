5e6a7e33f2544db398e218a796b6ca8cf58b818a
Diego Vaille <diego.vaille@ext.myfitnesspal.com>
2025-06-26 16:23:26 -0300

    Atualizacao do UplaodTestRunner

--- src/main/kotlin/br/com/storehouse/runner/UploadTestRunner.kt
+++ src/main/kotlin/br/com/storehouse/runner/UploadTestRunner.kt
@@ -1,18 +1,21 @@
-package br.com.storehouse
+package br.com.storehouse.runner
 
 import br.com.storehouse.service.DefaultStorageService
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
-import java.io.File
+import java.nio.file.Paths
 import java.util.*
 
+@Profile("!prod")
 @Component
 class UploadTestRunner(
     private val service: DefaultStorageService
 ) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        val imagem = File("/Users/diegovaille/Git/pib/storehouse-api/src/main/resources/images/logo.png").readBytes()
+        val imagePath = Paths.get("src/main/resources/images/logo.png").toFile()
+        val imagem = imagePath.readBytes()
         val url = service.uploadImagemProduto(UUID.randomUUID(), "789525790001", imagem)
         println("✅ Upload realizado com sucesso:")
         println("➡️ URL: $url")
