5def230215051e301c0e53e6079e1904f18a010a
Diego Vaille <diego.vaille@ext.myfitnesspal.com>
2025-11-30 13:34:46 -0300

    Atualizando liquibase e configurando para TZ America/SaoPaulo

--- src/main/kotlin/br/com/storehouse/StorehouseApplication.kt
+++ src/main/kotlin/br/com/storehouse/StorehouseApplication.kt
@@ -2,11 +2,13 @@ package br.com.storehouse
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 
 @SpringBootApplication
 class StorehouseApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"))
     runApplication<StorehouseApplication>(*args)
 }
